d0596d957452569c99d297e37a8013ca356cb326
Rob Peschke <rspesc31@gmail.com>
2021-01-28 08:50:42 -0700

    first commit with mods

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Robert on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
